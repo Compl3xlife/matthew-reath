ce235103f9cc8c5b0bfe120b8354c63bfd70ddaa
Compl3xlife <Compl3xlife@users.noreply.github.com>
2026-09-21 19:29:14 -0500

    Deploy portfolio to GitHub Pages.

---
+++ _next/static/6Dj_gq1tV1GT29-gvQEYI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/matthew-reath//_next/:path+",
+        "destination": "/matthew-reath/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
